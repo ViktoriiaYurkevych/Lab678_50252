89d92c34665c79e006368fa190a1e9159150d838
ViktoriiaYurkevych <vikysssik21@gmail.com>
2023-06-14 13:21:33 +0200

    Task 3 add json file saving

---
+++ json_file_conversions.py
@@ -0,0 +1,26 @@
+import json
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('input_file', help='Input file name')
+parser.add_argument('output_file', help='Output file name')
+args = parser.parse_args()
+
+input_file, output_file = args.input_file, args.output_file
+
+def load_json_file(input_file):
+    with open(input_file, 'r') as file:
+        try:
+            data = json.load(file)
+            return data
+        except json.JSONDecodeError as e:
+            print(f'Error while converting JSON: {e}')
+
+def save_json_file(data, output_file):
+    with open(output_file, 'w') as file:
+        json.dump(data, file, indent=4)
+
+data = load_json_file(input_file)
+if data:
+    print(f'Data that was loaded: {data}')
+    save_json_file(data, output_file)
